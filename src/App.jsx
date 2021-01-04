ed105fdbc292a57cf026b969fd9261c037327cd7
J3SUNG <wptjd6141@naver.com>
2021-01-03 21:01:20 -0800

    fixed background

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Greeting from "./function/Greeting";
 import Clock from "./function/Clock";
 import Background from "./function/Background";
-import ToDo from "./function/ToDo";
+// import ToDo from "./function/ToDo";
 import "./App.css";
 
 function App() {
@@ -14,10 +14,6 @@ function App() {
       <Greeting />
       <Clock />
 
-      <div className="js-greetings greetings"></div>
-      <form className="js-toDoForm">
-        <input type="text" placeholder="write a to do" />
-      </form>
       <ul className="js-toDoList"></ul>
       <span className="js-weather"></span>
     </div>
