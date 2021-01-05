4d8bc98f4e5c3ca5590c7a7f297209db77fda33d
J3SUNG <wptjd6141@naver.com>
2021-01-05 03:28:33 -0800

    weather

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,17 @@ import Greeting from "./function/Greeting";
 import Clock from "./function/Clock";
 import Background from "./function/Background";
 import ToDo from "./function/ToDo";
+import Weather from "./function/Weather";
 import "./App.css";
 
 function App() {
-  // document.body.style.backgroundImage = `url(${bg})`;
-  // document.body.style.backgroundImage = "url(/static/media/1.dcd9e70b.jpg)";
   return (
     <div>
       <Background />
       <Greeting />
       <Clock />
       <ToDo />
-
-      <ul className="js-toDoList"></ul>
-      <span className="js-weather"></span>
+      <Weather />
     </div>
   );
 }
